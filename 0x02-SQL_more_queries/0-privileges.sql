f7b4e4f5b79c448d9efd77ca652b6e0b35d780da
KingvonfrmO <2205481@students.kcau.ac.ke>
2025-04-25 19:06:00 +0300

    Assignment 2

---
+++ 0x02-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+USE hbtn_0c_0;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x02-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+USE hbtn_0d_2;
+CREATE TABLE IF NOT EXISTS `unique_id`(`id` INT default 1 unique, `name` VARCHAR(256));
